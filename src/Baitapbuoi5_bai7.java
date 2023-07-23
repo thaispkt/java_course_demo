f0a89fc788bf6794626448d5cf3843d6c31a76b3
Thai Nguyen <nguyenduythai@gmail.com>
2023-07-23 23:45:30 +0700

    Optimze execise Baitapbuoi5_bai8

--- src/Baitapbuoi5_bai7.java
+++ src/Baitapbuoi5_bai7.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-//Bài 6: Viết chương trình giải phương trình bậc nhất ax + b = 0 trong đó a, b là 2 số được người dùng nhập vào màn hình
+//Bài 7: Viết chương trình giải phương trình bậc nhất ax + b = 0 trong đó a, b là 2 số được người dùng nhập vào màn hình
 public class Baitapbuoi5_bai7 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -17,7 +17,7 @@ public class Baitapbuoi5_bai7 {
         float b = scanner.nextFloat();
         float x = -b / a;
         // print the value of 'x' with two decimal places
-        System.out.printf("The solution for the equation is x=%.2f", x);
+        System.out.printf("The solution for the equation is x = %.2f", x);
         scanner.close();
     }
 }
