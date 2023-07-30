db13a66c9528e726ae82ec70a0d6d25e1fda1e60
Thai Nguyen <nguyenduythai@gmail.com>
2023-07-30 19:35:34 +0700

    Finished Bai tap buoi 8 - bai 5

--- src/Baitapbuoi8_bai5/Student.java
+++ src/Baitapbuoi8_bai5/Student.java
@@ -6,10 +6,28 @@ package Baitapbuoi8_bai5;
  */
 public class Student {
     String name;
-    String studentID;
+    String id;
     float mathMark;
     float physicsMark;
     float chemistryMark;
-    float scoreAverage;
+    float averageScore;
     String rank;
+    public void calcScoreAverage() {
+        this.averageScore = (this.mathMark + this.physicsMark + this.chemistryMark)/3;
+    }
+    public void ranking() {
+        if(this.averageScore >= 9) {
+            this.rank = "Excellence";
+        } else if (this.averageScore >=8) {
+            this.rank = "Good";
+        } else if (this.averageScore >= 7) {
+            this.rank = "Fair";
+        } else if (this.averageScore >=6 ) {
+            this.rank = "Fair-Average";
+        } else if (this.averageScore >=5 ) {
+            this.rank = "Average";
+        } else {
+            this.rank = "Weak";
+        }
+    }
 }
--- src/Baitapbuoi8_bai5/StudentApp.java
+++ src/Baitapbuoi8_bai5/StudentApp.java
@@ -1,4 +1,7 @@
 package Baitapbuoi8_bai5;
+
+import java.util.Scanner;
+
 /*
 1)	Xây dựng chương trình cho phép người dùng nhập vào: Tên, Mã SV, điểm Toán, Lý Hóa. Thực hiện các nghiệp vụ dưới đây.
 •	Tính điểm trung bình từng sinh viên ( T + L + H)/3
@@ -6,6 +9,32 @@ package Baitapbuoi8_bai5;
  */
 public class StudentApp {
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter number of students: ");
+        int N = scanner.nextInt();
+        scanner.nextLine(); // Consume the left new line character in buffer
+        Student[] students = new Student[N];
+        for (int i = 0; i < students.length; i++) {
+            students[i] = new Student();
+            System.out.print("Enter student name: ");
+            students[i].name = scanner.nextLine();
+            System.out.print("Enter student id: ");
+            students[i].id = scanner.nextLine();
+            System.out.print("Enter the student's math mark: ");
+            students[i].mathMark = scanner.nextInt();
+            System.out.print("Enter the student's physic mark : ");
+            students[i].physicsMark = scanner.nextInt();
+            System.out.print("Enter the student's chemistry mark : ");
+            students[i].chemistryMark = scanner.nextInt();
+            scanner.nextLine();
+        }
+        System.out.println("Student Ranking:");
+        System.out.printf("%-20s %-10s %-15s %s%n", "Name", "ID", "Average Score", "Rank");
+        for (Student student: students) {
+            student.calcScoreAverage();
+            student.ranking();
+            System.out.printf("%-20s %-10s %-15.2f %s%n", student.name, student.id, student.averageScore, student.rank);
+        }
+        scanner.close();
     }
 }
