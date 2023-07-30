61d7a4e39cc852901963b3b670d1947be6d0a081
Thai Nguyen <nguyenduythai@gmail.com>
2023-07-30 13:13:27 +0700

    Add menu of exercise 5

--- src/Baitapbuoi5/Baitapbuoi5_bai4.java
+++ src/Baitapbuoi5/Baitapbuoi5_bai4.java
@@ -7,39 +7,53 @@ public class Baitapbuoi5_bai4 {
     public static void main(String[] args) {
         calculator();
     }
+
     public static void calculator() {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter a: ");
         int a = scanner.nextInt();
         System.out.print("Enter b: ");
         int b = scanner.nextInt();
-        System.out.print("Enter operator: add/substract/multiplier/divide (1/2/3/4): ");
-        int operator = scanner.nextInt();
-        switch (operator) {
-            case 1:
-                int sum = a + b;
-                System.out.println("a + b = " + sum);
-                break;
-            case 2:
-                int diff = a - b;
-                System.out.print("a - b = " + diff);
-                break;
-            case 3:
-                int mul = a * b;
-                System.out.printf("a * b = " + mul);
-                break;
-            case 4:
-                while(b == 0) {
-                    System.out.print("Error: Cannot divide by zero. Please re-enter b: ");
-                    b = scanner.nextInt();
-                }
-                float div = (float) a / b;
-                System.out.printf("a / b = %.2f", div);
-                break;
-            default:
-                System.out.println("Invalid operator");
-                break;
-        }
+        System.out.println("Menu:");
+        System.out.println("1. Addition");
+        System.out.println("2. Subtraction");
+        System.out.println("3. Multiplication");
+        System.out.println("4. Division");
+        System.out.println("0. Exit");
+        int choice = 0;
+        do {
+            System.out.print("Enter choice: ");
+            choice = scanner.nextInt();
+            switch (choice) {
+                case 1:
+                    int sum = a + b;
+                    System.out.println("a + b = " + sum);
+                    break;
+                case 2:
+                    int diff = a - b;
+                    System.out.println("a - b = " + diff);
+                    break;
+                case 3:
+                    int mul = a * b;
+                    System.out.println("a * b = " + mul);
+                    break;
+                case 4:
+                    while (b == 0) {
+                        System.out.print("Error: Cannot divide by zero. Please re-enter b: ");
+                        b = scanner.nextInt();
+                    }
+                    float div = (float) a / b;
+                    System.out.printf("a / b = %.2f\n", div);
+                    break;
+                case 0:
+                    System.out.println("Exiting program");
+                    break;
+                default:
+                    System.out.println("Invalid choice");
+                    break;
+            }
+        } while (choice != 0);
+
         scanner.close();
     }
 }
