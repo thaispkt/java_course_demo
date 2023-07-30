8308e1a0b7a86f860ee4c18f6b8d66c7480e2391
Thai Nguyen <nguyenduythai@gmail.com>
2023-07-30 16:33:09 +0700

    Remove directory out/production

---
+++ src/Baitapbuoi8_bai5/StudentApp.java
@@ -0,0 +1,11 @@
+package Baitapbuoi8_bai5;
+/*
+1)	Xây dựng chương trình cho phép người dùng nhập vào: Tên, Mã SV, điểm Toán, Lý Hóa. Thực hiện các nghiệp vụ dưới đây.
+•	Tính điểm trung bình từng sinh viên ( T + L + H)/3
+•	Xếp loại từng sinh viên theo: >=9 -> Xuất Sắc, 9<Giỏi<=8, 8<Khá<=7, <7<=6, <=5, còn lại Yếu.
+ */
+public class StudentApp {
+    public static void main(String[] args) {
+
+    }
+}
