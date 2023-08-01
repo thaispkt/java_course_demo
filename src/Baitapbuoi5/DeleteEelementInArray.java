0346e2a0ffbf4164fbeb0dbda95ad546455174a8
Thai Nguyen <nguyenduythai@gmail.com>
2023-08-01 08:49:44 +0700

    Delete student based on ID

--- src/Baitapbuoi5/DeleteEelementInArray.java
+++ src/Baitapbuoi5/DeleteEelementInArray.java
@@ -3,6 +3,12 @@ package Baitapbuoi5;
 import java.util.ArrayList;
 
 public class DeleteEelementInArray {
+//    In Java, the primitive array type (e.g., int[], double[], etc.) does not have a built-in remove method. The
+//    remove method you mentioned is not available for primitive arrays because arrays in Java have a fixed size once they are created, and their length cannot be changed dynamically.
+//
+//    The remove method is commonly used with dynamic data structures like ArrayList, which is part of the Java Collections Framework. ArrayList allows you to add, remove, and resize elements dynamically, unlike primitive arrays.
+//
+//    Here's an example of how you can use the remove method with an ArrayList:
     public static void main(String[] args) {
         ArrayList<Integer> list = new ArrayList<>();
         list.add(1);
