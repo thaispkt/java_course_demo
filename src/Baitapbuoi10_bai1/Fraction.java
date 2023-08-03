c653bdf5ce7b79d8d1595e0b2a1db50bda3d1feb
Thai Nguyen <nguyenduythai@gmail.com>
2023-08-03 21:11:12 +0700

    Input scanner

--- src/Baitapbuoi10_bai1/Fraction.java
+++ src/Baitapbuoi10_bai1/Fraction.java
@@ -9,7 +9,8 @@ Xây dựng lớp Phanso gồm:
 •	Hàm Khởi Tạo Không Tham Số,
 •	Nhập , Xuất
 •	Cong(), Tru(), Nhan(), Chia()
-Tính Tổng, Hiệu, Tích, Thương 2 phân số tham số nhận vào là đối tượng (Phân Số) rồi in kết quả ra màn hình.
+Tính Tổng, Hiệu, Tích, Thương 2 phân số tham số nhận vào là đối tượng (Phân Số) rồi in kết quả ra
+ màn hình.
 Phân số: Fraction
 Tử số: numerator
 mẫu số: denominator
@@ -17,49 +18,59 @@ mẫu số: denominator
 public class Fraction {
     private int numerator;
     private int denominator;
+
     public Fraction() {
         numerator = 0;
         denominator = 1;
-    };
-    public Fraction(int numerator, int denominator) {
-        this.numerator=numerator;
-        this.denominator=denominator;
     }
+
+    ;
+
+    public Fraction(int numerator, int denominator) {
+        this.numerator = numerator;
+        this.denominator = denominator;
+    }
+
     public void importData(int i) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter numerator of fraction " + i + ": " );
+        System.out.print("Enter numerator of fraction " + i + ": ");
         this.numerator = scanner.nextInt();
         System.out.print("Enter denominator of fraction " + i + ": ");
         this.denominator = scanner.nextInt();
-        scanner.close();
+//        scanner.close();
     }
+
     public String toString() {
-        return(this.numerator +"/"+this.denominator);
+        return (this.numerator + "/" + this.denominator);
     }
-    public Fraction add( Fraction frac2) {
+
+    public Fraction add(Fraction frac2) {
         int outNumerator =
-                (this.numerator* frac2.denominator) + (this.denominator*frac2.numerator);
-        int outDenominator = this.denominator*frac2.denominator;
-        Fraction resultFraction = new Fraction(outNumerator,outDenominator);
+                (this.numerator * frac2.denominator) + (this.denominator * frac2.numerator);
+        int outDenominator = this.denominator * frac2.denominator;
+        Fraction resultFraction = new Fraction(outNumerator, outDenominator);
         return resultFraction;
     }
+
     public Fraction subtract(Fraction frac2) {
         int outNumerator =
-                (this.numerator* frac2.denominator) - (this.denominator*frac2.numerator);
-        int outDenominator = this.denominator*frac2.denominator;
-        Fraction resultFraction = new Fraction(outNumerator,outDenominator);
+                (this.numerator * frac2.denominator) - (this.denominator * frac2.numerator);
+        int outDenominator = this.denominator * frac2.denominator;
+        Fraction resultFraction = new Fraction(outNumerator, outDenominator);
         return resultFraction;
     }
+
     public Fraction multiply(Fraction frac2) {
-        int outNumerator = this.numerator*frac2.numerator;
-        int outDenominator = this.denominator*frac2.denominator;
-        Fraction resultFraction = new Fraction(outNumerator,outDenominator);
+        int outNumerator = this.numerator * frac2.numerator;
+        int outDenominator = this.denominator * frac2.denominator;
+        Fraction resultFraction = new Fraction(outNumerator, outDenominator);
         return resultFraction;
     }
+
     public Fraction divide(Fraction frac2) {
-        int outNumerator = this.numerator*frac2.denominator;
-        int outDenominator = this.denominator*frac2.numerator;
-        Fraction resultFraction = new Fraction(outNumerator,outDenominator);
+        int outNumerator = this.numerator * frac2.denominator;
+        int outDenominator = this.denominator * frac2.numerator;
+        Fraction resultFraction = new Fraction(outNumerator, outDenominator);
         return resultFraction;
     }
 }
