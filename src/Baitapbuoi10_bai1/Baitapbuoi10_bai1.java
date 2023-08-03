41c39487920201211d0c3d8530f6e0fd8a8a2802
Thai Nguyen <nguyenduythai@gmail.com>
2023-08-03 20:28:10 +0700

    Code alignment

--- src/Baitapbuoi10_bai1/Baitapbuoi10_bai1.java
+++ src/Baitapbuoi10_bai1/Baitapbuoi10_bai1.java
@@ -17,10 +17,10 @@ public class Baitapbuoi10_bai1 {
     public static void main(String[] args) {
         Fraction fraction1 = new Fraction(2, 3);
         Fraction fraction2 = new Fraction(1, 4);
-        Fraction fractionSum = fraction1.add(fraction2);
+        Fraction fractionSum  = fraction1.add(fraction2);
         Fraction fractionDiff = fraction1.subtract(fraction2);
-        Fraction fractionMul = fraction1.multiply(fraction2);
-        Fraction fractionDiv = fraction1.divide(fraction2);
+        Fraction fractionMul  = fraction1.multiply(fraction2);
+        Fraction fractionDiv  = fraction1.divide(fraction2);
         System.out.println("Sum of 2 fractions: " + fractionSum.toString());
         System.out.println("Diff of 2 fractions: " + fractionDiff.toString());
         System.out.println("Product of 2 fractions: " + fractionMul.toString());
