5eda43a277e0202dd6dfd6bef05d84c51ce9f4f5
thaispkt <nguyenduythai@gmail.com>
2023-10-01 22:38:06 +0700

    Submit bai tap buoi 25

--- MySQL_exercises/SQL_exercise_session_25.sql
+++ MySQL_exercises/SQL_exercise_session_25.sql
@@ -2,55 +2,55 @@ CREATE DATABASE ThucTap;
 -- DROP DATABASE ThucTap;
 USE ThucTap;
 
-CREATE TABLE TBLKhoa (
-    Makhoa char(10) primary key,
-    Tenkhoa char(30),
-    Dienthoai char(10)
+CREATE TABLE Khoa (
+    MaKhoa char(10) primary key,
+    TenKhoa char(30),
+    DienThoai char(10)
 );
 
-CREATE TABLE TBLGiangVien(
-    Magv int primary key,
-    Hotengv char(30),
+CREATE TABLE GiangVien(
+    MaGV int primary key,
+    HoTenGv char(30),
     Luong decimal(5, 2),
-    Makhoa char(10),
-    foreign key (Makhoa) references TBLKhoa(Makhoa)
+    MaKhoa char(10),
+    foreign key (MaKhoa) references Khoa(MaKhoa)
 );
--- DROP TABLE TBLGiangVien;
+-- DROP TABLE GiangVien;
 
-CREATE TABLE TBLSinhVien(
-    Masv int primary key,
-    Hotensv char(40),
-    Makhoa char(10), 
-    Namsinh int,
-    Quequan char(30),
-    foreign key (Makhoa) references TBLKhoa(Makhoa)
+CREATE TABLE SinhVien(
+    MaSV int primary key,
+    HoTenSV char(40),
+    MaKhoa char(10), 
+    NamSinh int,
+    QueQuan char(30),
+    foreign key (MaKhoa) references Khoa(MaKhoa)
 );
 
-CREATE TABLE TBLDeTai(
-    Madt char(10) primary key,
-    Tendt char(30),
+CREATE TABLE DeTai(
+    MaDeTai char(10) primary key,
+    TenDeTai char(30),
     Kinhphi int,
-    Noithuctap char(30)
+    NoiThucTap char(30)
 );
 
-CREATE TABLE TBLHuongDan(
-    Masv int primary key,
-    Madt char(10),
-    Magv int,
+CREATE TABLE HuongDan(
+    MaSV int primary key,
+    MaDeTai char(10),
+    MaGV int,
     KetQua decimal(5, 2),    
-    foreign key (Madt) references TBLDeTai(Madt),
-	foreign key (Magv) references TBLGiangVien(Magv)    
+    foreign key (MaDeTai) references DeTai(MaDeTai),
+	foreign key (MaGV) references GiangVien(MaGV)    
 );
 
 INSERT INTO
-    TBLKhoa
+    Khoa
 VALUES
     ('Geo', 'Dia ly va QLTN', 3855413),
     ('Math', 'Toan', 3855411),
     ('Bio', 'Cong nghe Sinh hoc', 3855412);
 
 INSERT INTO
-    TBLGiangVien
+    GiangVien
 VALUES
     (11, 'Thanh Binh', 700, 'Geo'),
     (12, 'Thu Huong', 500, 'Math'),
@@ -59,7 +59,7 @@ VALUES
     (15, 'Tran Son', 900, 'Math');
 
 INSERT INTO
-    TBLSinhVien
+    SinhVien
 VALUES
     (1, 'Le Van Son', 'Bio', 1990, 'Nghe An'),
     (2, 'Nguyen Thi Mai', 'Geo', 1990, 'Thanh Hoa'),
@@ -71,7 +71,7 @@ VALUES
     (8, 'Hoang Van Duc', 'Bio', 1992, 'Nghe An');
 
 INSERT INTO
-    TBLDeTai
+    DeTai
 VALUES
     ('Dt01', 'GIS', 100, 'Nghe An'),
     ('Dt02', 'ARC GIS', 500, 'Nam Dinh'),
@@ -79,7 +79,7 @@ VALUES
     ('Dt04', 'MAP', 300, 'Quang Binh');
 
 INSERT INTO
-    TBLHuongDan
+    HuongDan
 VALUES
     (1, 'Dt01', 13, 8),
     (2, 'Dt03', 14, 0),
@@ -90,71 +90,127 @@ VALUES
     (8, 'Dt03', 15, 6);
  
    
--- 1.	Đưa ra thông tin gồm mã số, họ tên và tên khoa của tất cả các giảng viên
-	SELECT g.Magv,g.Hotengv,k.Tenkhoa  
-	FROM TBLGiangVien g
-	JOIN TBLKhoa k ON g.Makhoa = k.Makhoa;
--- 2.	Đưa ra thông tin gồm mã số, họ tên và tên khoa của các giảng viên của khoa ‘DIA LY va QLTN’  
-	SELECT g.Magv,g.Hotengv,k.Tenkhoa  
-	FROM TBLGiangVien g
-	JOIN TBLKhoa k ON g.Makhoa = k.Makhoa 
-	WHERE k.Tenkhoa = 'Dia ly va QLTN';
+-- 1.	Đưa ra thông tin gồm mã số, họ tên và tên Khoa của tất cả các giảng viên
+	SELECT g.MaGV,g.HoTenGv,k.TenKhoa  
+	FROM GiangVien g
+	JOIN Khoa k ON g.MaKhoa = k.MaKhoa;
+
+-- 2.	Đưa ra thông tin gồm mã số, họ tên và tên Khoa của các giảng viên của Khoa ‘DIA LY va QLTN’  
+	SELECT g.MaGV,g.HoTenGv,k.TenKhoa  
+	FROM GiangVien g
+	JOIN Khoa k ON g.MaKhoa = k.MaKhoa 
+	WHERE k.TenKhoa = 'Dia ly va QLTN';
 	
--- 3.	Cho biết số sinh viên của khoa ‘Cong nghe Sinh hoc’
-SELECT k.Tenkhoa, COUNT(s.Masv) AS SoSinhVien
-FROM TBLSinhVien s
-JOIN TBLKhoa k ON s.Makhoa = k.Makhoa
-WHERE k.Tenkhoa = 'Cong nghe Sinh hoc';
+-- 3.	Cho biết số sinh viên của Khoa ‘Cong nghe Sinh hoc’
+	SELECT k.TenKhoa, COUNT(s.MaSV) AS SoSinhVien
+	FROM SinhVien s
+	JOIN Khoa k ON s.MaKhoa = k.MaKhoa
+	WHERE k.TenKhoa = 'Cong nghe Sinh hoc';
 
--- 4.	Đưa ra danh sách gồm mã số, họ tên và tuổi của các sinh viên khoa ‘Toan’
-SELECT s.Masv, s.Hotensv, YEAR(CURRENT_DATE()) - s.Namsinh AS Tuoi
-FROM TBLSinhVien s
-JOIN TBLKhoa k ON s.Makhoa = k.Makhoa
-WHERE k.Tenkhoa = 'Toan';
+-- 4.	Đưa ra danh sách gồm mã số, họ tên và tuổi của các sinh viên Khoa ‘Toan’
+	SELECT s.MaSV, s.HoTenSV, YEAR(CURRENT_DATE()) - s.NamSinh AS Tuoi
+	FROM SinhVien s
+	JOIN Khoa k ON s.MaKhoa = k.MaKhoa
+	WHERE k.TenKhoa = 'Toan';
 
--- 5.	Cho biết số giảng viên của khoa ‘Cong nghe Sinh hoc’
-SELECT k.Tenkhoa, COUNT(g.Magv) AS SoGiangVien
-FROM TBLGiangVien g
-JOIN TBLKhoa k ON g.Makhoa = k.Makhoa 
-WHERE k.Tenkhoa = 'Cong nghe Sinh hoc';
+-- 5.	Cho biết số giảng viên của Khoa ‘Cong nghe Sinh hoc’
+	SELECT k.TenKhoa, COUNT(g.MaGV) AS SoGiangVien
+	FROM GiangVien g
+	JOIN Khoa k ON g.MaKhoa = k.MaKhoa 
+	WHERE k.TenKhoa = 'Cong nghe Sinh hoc';
 
 -- 6.	Cho biết thông tin về sinh viên không tham gia thực tập
-SELECT * FROM TBLSinhVien;
-SELECT * FROM TBLHuongDan;
-SELECT s.Masv, s.Hotensv, s.Makhoa, s.Namsinh, s.Quequan
-FROM TBLSinhVien s
-LEFT JOIN TBLHuongDan h ON s.Masv = h.Masv
-WHERE h.Masv IS NULL;
+	SELECT * FROM SinhVien;
+	SELECT * FROM HuongDan;
+	SELECT s.MaSV, s.HoTenSV, s.MaKhoa, s.NamSinh, s.QueQuan
+	FROM SinhVien s
+	LEFT JOIN HuongDan h ON s.MaSV = h.MaSV
+	WHERE h.MaSV IS NULL;
+	
+	SELECT *
+	FROM SinhVien s
+	LEFT JOIN HuongDan h ON s.MaSV = h.MaSV;
+	
+	SELECT *
+	FROM SinhVien s
+	JOIN HuongDan h ON s.MaSV = h.MaSV;
 
-SELECT *
-FROM TBLSinhVien s
-LEFT JOIN TBLHuongDan h ON s.Masv = h.Masv;
+-- 7.	Đưa ra mã Khoa, tên Khoa và số giảng viên của mỗi Khoa
+	SELECT k.MaKhoa, k.TenKhoa , COUNT(g.MaKhoa) AS SoGiangVien
+	FROM GiangVien g
+	JOIN Khoa k ON g.MaKhoa = k.MaKhoa 
+	GROUP BY g.MaKhoa;
 
-SELECT *
-FROM TBLSinhVien s
-JOIN TBLHuongDan h ON s.Masv = h.Masv;
-
--- 7.	Đưa ra mã khoa, tên khoa và số giảng viên của mỗi khoa
-SELECT k.Makhoa, k.Tenkhoa , COUNT(g.Makhoa) AS SoGiangVien
-FROM TBLGiangVien g
-JOIN TBLKhoa k ON g.Makhoa = k.Makhoa 
-GROUP BY g.Makhoa;
-
--- 8.	Cho biết số điện thoại của khoa mà sinh viên có tên ‘Le Van Son’ đang theo học
-SELECT * FROM TBLSinhVien;
-SELECT k.Tenkhoa, k.Dienthoai AS SoDienThoai
-FROM TBLSinhVien sv
-JOIN TBLKhoa k ON sv.Makhoa = k.Makhoa 
-WHERE sv.Hotensv = 'Le Van Son'
+-- 8.	Cho biết số điện thoại của Khoa mà sinh viên có tên ‘Le Van Son’ đang theo học
+	SELECT * FROM SinhVien;
+	SELECT k.TenKhoa, k.DienThoai AS SoDienThoai
+	FROM SinhVien sv
+	JOIN Khoa k ON sv.MaKhoa = k.MaKhoa 
+	WHERE sv.HoTenSV = 'Le Van Son';
 
 -- II
 -- 1.	Cho biết mã số và tên của các đề tài do giảng viên ‘Tran son’ hướng dẫn
--- 2.	Cho biết tên đề tài không có sinh viên nào thực tập
--- 3.	Cho biết mã số, họ tên, tên khoa của các giảng viên hướng dẫn từ 3 sinh viên trở lên.
+	SELECT dt.MaDeTai, dt.TenDeTai
+	FROM GiangVien gv
+	JOIN HuongDan hd ON gv.MaGV = hd.MaGV
+	JOIN DeTai dt ON hd.MaDeTai = dt.MaDeTai
+	WHERE gv.HoTenGv = 'Tran Son';
+
+-- 2.	Cho biết tên đề tài không có sinh viên nào được thực tập
+	SELECT TenDeTai
+	FROM DeTai
+	WHERE MaDeTai NOT IN (SELECT MaDeTai FROM HuongDan);
+
+-- 3.	Cho biết mã số, họ tên, tên Khoa của các giảng viên hướng dẫn từ 3 sinh viên trở lên.
+	SELECT gv.MaGV, gv.HoTenGV, k.TenKhoa
+	FROM GiangVien gv
+	JOIN Khoa k ON gv.MaKhoa = k.MaKhoa
+	WHERE gv.MaGV IN (
+	    SELECT MaGV
+	    FROM HuongDan
+	    GROUP BY MaGV
+	    HAVING COUNT(MaSV) >= 3
+	);
+
 -- 4.	Cho biết mã số, tên đề tài của đề tài có kinh phí cao nhất
+	SELECT MaDeTai, TenDeTai, KinhPhi
+	FROM DeTai
+	WHERE KinhPhi = (SELECT MAX(KinhPhi) FROM DeTai);
+
 -- 5.	Cho biết mã số và tên các đề tài có nhiều hơn 2 sinh viên tham gia thực tập
--- 6.	Đưa ra mã số, họ tên và điểm của các sinh viên khoa ‘DIALY và QLTN’
--- 7.	Đưa ra tên khoa, số lượng sinh viên của mỗi khoa
+	SELECT dt.MaDeTai, dt.TenDeTai
+	FROM DeTai dt
+	JOIN HuongDan hd ON dt.MaDeTai = hd.MaDeTai
+	GROUP BY dt.MaDeTai, dt.TenDeTai
+	HAVING COUNT(hd.MaSV) > 2;
+
+-- 6.	Đưa ra mã số, họ tên và điểm của các sinh viên Khoa ‘DIA LY và QLTN’
+	SELECT sv.MaSV, sv.HoTenSV, hd.KetQua
+	FROM SinhVien sv
+	LEFT JOIN HuongDan hd ON sv.MaSV = hd.MaSV
+	WHERE sv.MaKhoa = (SELECT MaKhoa FROM Khoa WHERE TenKhoa = 'Dia ly va QLTN');
+
+-- 7.	Đưa ra tên Khoa, số lượng sinh viên của mỗi Khoa
+	SELECT k.TenKhoa, COUNT(sv.MaSV) AS SoLuongSV
+	FROM Khoa k
+	LEFT JOIN SinhVien sv ON k.MaKhoa = sv.MaKhoa
+	GROUP BY k.TenKhoa;
+
 -- 8.	Cho biết thông tin về các sinh viên thực tập tại quê nhà
+	SELECT sv.MaSV, sv.Hotensv, sv.QueQuan, dt.NoiThucTap, dt.TenDeTai
+	FROM SinhVien sv
+	JOIN HuongDan hd ON sv.MaSV = hd.MaSV
+	JOIN DeTai dt ON hd.MaDeTai = dt.MaDeTai
+	WHERE sv.QueQuan = dt.NoiThucTap;
+
 -- 9.	Hãy cho biết thông tin về những sinh viên chưa có điểm thực tập
+	SELECT sv.MaSV, sv.HoTenSV, hd.KetQua
+	FROM SinhVien sv
+	LEFT JOIN HuongDan hd ON sv.MaSV = hd.MaSV
+	WHERE hd.KetQua IS NULL;
+
 -- 10.	Đưa ra danh sách gồm mã số, họ tên các sinh viên có điểm thực tập bằng 0
+	SELECT sv.MaSV, sv.HoTenSV
+	FROM SinhVien sv
+	JOIN HuongDan hd ON sv.MaSV = hd.MaSV
+	WHERE hd.KetQua = 0;
